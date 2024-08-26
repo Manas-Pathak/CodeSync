119c66108aab99bdfbd4cad957fb90b1fc1da194
Manas-Pathak <pathakmanas111@gmail.com>
2024-08-26 16:26:05 +0530

    change in server

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ const io = new Server(server, {
     }
 });
 
-app.use(express.static('build'));
-app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.use(express.static('build'));
+// app.use((req, res, next) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 //-----Deployement-------
 
